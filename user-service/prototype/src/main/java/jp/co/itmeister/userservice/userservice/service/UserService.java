21ccc1e68db733a39b1cbb67d0c55ded52c112be
kijima-riku <riku.bulls.0116@icloud.com>
2024-06-09 04:48:08 +0900

    [add] サービス層 createUser関数の追加

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/service/UserService.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/service/UserService.java
@@ -1,5 +1,22 @@
 package jp.co.itmeister.userservice.userservice.service;
 
+import jp.co.itmeister.userservice.userservice.model.UserModel;
+import jp.co.itmeister.userservice.userservice.repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserModel createUser(UserModel user) {
+        return userRepository.save(user);
+    }
 }
