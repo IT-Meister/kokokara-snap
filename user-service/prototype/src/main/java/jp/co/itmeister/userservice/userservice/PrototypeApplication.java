51a302a9aa501186da91d6c04265526a4b356897
kijima-riku <riku.bulls.0116@icloud.com>
2024-06-08 23:57:54 +0900

    ディレクトリ構造の変更

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/PrototypeApplication.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/PrototypeApplication.java
@@ -1,4 +1,4 @@
-package com.itmeister.prototype;
+package jp.co.itmeister.userservice.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
