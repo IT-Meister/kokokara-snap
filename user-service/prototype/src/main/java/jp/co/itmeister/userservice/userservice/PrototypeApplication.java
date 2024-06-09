b435d26f842b0bff34b046772f313cf5d28e89af
kijima-riku <riku.bulls.0116@icloud.com>
2024-06-10 00:40:53 +0900

    mainClass タイムゾーンの設定

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/PrototypeApplication.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/PrototypeApplication.java
@@ -1,10 +1,13 @@
 package jp.co.itmeister.userservice.userservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication
@@ -18,5 +21,10 @@ public class PrototypeApplication {
 	@RequestMapping("/")
 	public String hello() {
 		return "Hello Spring Boot on Docker!";
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("JST"));
 	}
 }
