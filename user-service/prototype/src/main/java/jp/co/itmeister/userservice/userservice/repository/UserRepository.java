e7667bb63ea1516eae00d1d75b5d7d643bcfc95f
kijima-riku <riku.bulls.0116@icloud.com>
2024-08-23 15:29:31 +0900

    [fix] rename

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/repository/UserRepository.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package jp.co.itmeister.userservice.userservice.repository;
 
-import jp.co.itmeister.userservice.userservice.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import jp.co.itmeister.userservice.userservice.entity.UserEntity;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 }
