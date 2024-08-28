540d5a929c71006cee9c7976e7c1d579ce3d5f2c
kijima-riku <riku.bulls.0116@icloud.com>
2024-08-28 19:00:45 +0900

    [add] Logic in post service.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/service/PostService.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/service/PostService.java
@@ -1,5 +1,25 @@
 package jp.co.itmeister.userservice.userservice.service;
 
+import jp.co.itmeister.userservice.userservice.entity.PostEntity;
+import jp.co.itmeister.userservice.userservice.repository.PostRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class PostService {
     
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Optional<PostEntity> showPost (Long id) {
+        return postRepository.findById(id);
+    }
 }
