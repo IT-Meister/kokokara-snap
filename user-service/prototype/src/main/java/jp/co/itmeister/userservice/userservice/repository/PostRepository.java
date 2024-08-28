528e9e7b97bf6a9396e7b095c58a5e0d3dc20707
kijima-riku <riku.bulls.0116@icloud.com>
2024-08-28 18:47:35 +0900

    [add] Logic in post repositoy.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/repository/PostRepository.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/repository/PostRepository.java
@@ -1,5 +1,11 @@
 package jp.co.itmeister.userservice.userservice.repository;
 
-public class PostRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.itmeister.userservice.userservice.entity.PostEntity;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
 }
