0c9ff3f24d2360586ab610e3be3c202d603eaffc
kijima-riku <riku.bulls.0116@icloud.com>
2024-08-28 18:37:01 +0900

    [add] Logic in post controller.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/PostController.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/PostController.java
@@ -1,25 +1,30 @@
 package jp.co.itmeister.userservice.userservice.controller;
 
-import jakarta.validation.Valid;
 import jp.co.itmeister.userservice.userservice.entity.PostEntity;
 import jp.co.itmeister.userservice.userservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
     
-    public final PostService postService;
+    private final PostService postService;
 
     @Autowired
-    public PostController (PostService postService){
+    public PostController(PostService postService){
         this.postService = postService;
     }
 
-    // @GetMapping
+    @GetMapping("/{post_id}")
+    public ResponseEntity<PostEntity> showPost(@PathVariable("post_id") Long id) {
+            PostEntity post = postService.showPost(id);
+            return ResponseEntity.ok(post);
+    }
 }
