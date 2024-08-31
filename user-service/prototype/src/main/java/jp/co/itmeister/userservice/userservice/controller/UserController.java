d1addfd7a0712024c71c7cf11c8dc7de6f0722a6
kijima-riku <riku.bulls.0116@icloud.com>
2024-08-31 17:03:46 +0900

    [add] routing in user controller.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/UserController.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/UserController.java
@@ -4,8 +4,11 @@ import jakarta.validation.Valid;
 import jp.co.itmeister.userservice.userservice.entity.UserEntity;
 import jp.co.itmeister.userservice.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,12 @@ public class UserController {
     @Autowired
     public UserController (UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<UserEntity> showUser (@PathVariable("user_id") Long id) {
+        UserEntity user = userService.showUser(id);
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping
