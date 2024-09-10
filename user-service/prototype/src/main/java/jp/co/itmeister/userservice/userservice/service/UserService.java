659a5f525a37a3015ebbd84e6d4217be7c2773c6
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-11 00:07:06 +0900

    [fix] user service

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/service/UserService.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/service/UserService.java
@@ -5,6 +5,8 @@ import jp.co.itmeister.userservice.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
@@ -16,11 +18,11 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public UserEntity showUser (Long id ) {
-        Optional<UserEntity> user = userRepository.findById(id);
-        return user.orElseThrow();
-    }
 
+public UserEntity showUser(Long id) {
+    return userRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+}
     public UserEntity createUser(UserEntity user) {
         return userRepository.save(user);
     }
