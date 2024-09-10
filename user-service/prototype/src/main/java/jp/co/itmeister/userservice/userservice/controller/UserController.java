1f9d516dfb9ccd05f95381e30ecb28c369f751e2
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-11 01:38:40 +0900

    [fix] response part in user controller.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/UserController.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/UserController.java
@@ -28,7 +28,17 @@ public class UserController {
     public ResponseEntity<?> showUser(@PathVariable("user_id") Long id) {
         try {
             UserEntity user = userService.showUser(id);
-            return ResponseEntity.ok(user);
+
+            //都道府県名を含める
+            Map<String, Object> response = Map.of(
+                "id", user.getId(),
+                "displayName", user.getDisplayName(),
+                "userName", user.getUserName(),
+                "email", user.getEmail(),
+                "prefecture", user.getPrefecture().getName()
+            );
+
+            return ResponseEntity.ok(response);
         } catch (NoSuchElementException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Map.of("error", "User not found"));
