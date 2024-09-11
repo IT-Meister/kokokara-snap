4f333bf531869971e20334359cd379eb6bca4992
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-11 23:48:31 +0900

    [add] indexPost logic in controller.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/PostController.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/PostController.java
@@ -2,12 +2,15 @@ package jp.co.itmeister.userservice.userservice.controller;
 
 import jp.co.itmeister.userservice.userservice.entity.PostEntity;
 import jp.co.itmeister.userservice.userservice.service.PostService;
+
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,6 +23,19 @@ public class PostController {
     @Autowired
     public PostController(PostService postService){
         this.postService = postService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostEntity>> indexPost(@RequestParam(value = "prefecture", required = false) Integer prefecture) {
+        List<PostEntity> posts;
+        if (prefecture != null) {
+            // 都道府県指定あり
+            posts = postService.findByPrefecture(prefecture);
+        } else {
+            // 都道府県指定なし
+            posts = postService.findAllPosts();
+        }
+        return ResponseEntity.ok(posts);
     }
 
     @GetMapping("/{post_id}")
