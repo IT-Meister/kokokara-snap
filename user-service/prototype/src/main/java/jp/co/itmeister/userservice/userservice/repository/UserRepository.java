fb3c06f2b07b33aeaadee3134c515b390add16f9
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-11 16:54:09 +0900

    [add] findByEmail in repository.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/repository/UserRepository.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package jp.co.itmeister.userservice.userservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import jp.co.itmeister.userservice.userservice.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+         Optional<UserEntity> findByEmail(String email);
 }
