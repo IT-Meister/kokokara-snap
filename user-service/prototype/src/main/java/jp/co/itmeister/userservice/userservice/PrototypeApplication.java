022e3b8dd354a0c3746635b7276971923aa4b4f6
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-12 17:01:03 +0900

    CORS対策

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/PrototypeApplication.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/PrototypeApplication.java
@@ -3,9 +3,12 @@ package jp.co.itmeister.userservice.userservice;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.TimeZone;
 
@@ -26,5 +29,21 @@ public class PrototypeApplication {
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("JST"));
+	}
+
+	//CORS対策
+	 @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+                    .maxAge(3600);
+            }
+        };
 	}
 }
