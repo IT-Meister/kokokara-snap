b2d6b01167399ad8534961003480f247b8c78e70
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-19 03:26:41 +0900

    [add] prefecture entity class.

---
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/entity/PrefectureEntity.java
@@ -0,0 +1,49 @@
+package jp.co.itmeister.userservice.userservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "prefectures")
+public class PrefectureEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    @Column(nullable = false , length = 64)
+    private String name;
+
+    @OneToMany(mappedBy = "prefecture")
+    private List<CityEntity> cities;
+
+    //getter & setter
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CityEntity> getCities() {
+        return cities;
+    }
+
+    public void setCities(List<CityEntity> cities) {
+        this.cities = cities;
+    }
+}
