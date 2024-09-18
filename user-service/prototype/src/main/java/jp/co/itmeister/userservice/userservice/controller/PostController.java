094a3683165c0dbce64097758787f97e2b76f20d
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-19 03:25:42 +0900

    [add] createPost method in controller.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/PostController.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/PostController.java
@@ -1,5 +1,6 @@
 package jp.co.itmeister.userservice.userservice.controller;
 
+import jp.co.itmeister.userservice.userservice.dto.PostDto;
 import jp.co.itmeister.userservice.userservice.entity.PostEntity;
 import jp.co.itmeister.userservice.userservice.responseBuilder.ResponseBuilder;
 import jp.co.itmeister.userservice.userservice.service.PostService;
@@ -13,9 +14,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/post")
@@ -31,7 +37,7 @@ public class PostController {
     }
 
     @GetMapping("/home")
-    public ResponseEntity<Map<String , Object>> indexPost(@RequestParam(value = "prefecture", required = false) Integer prefecture) {
+    public ResponseEntity<Map<String , Object>> indexPost(@RequestParam(value = "prefecture", required = false) Short prefecture) {
         List<PostEntity> posts;
         if (prefecture != null) {
             // 都道府県指定あり
@@ -52,5 +58,17 @@ public class PostController {
             } else {
                 return responseBuilder.buildErrorResponse("Post was not found.", HttpStatus.NOT_FOUND);
             }
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String , Object>> createPost (@Valid @RequestBody PostDto requestPost) {
+        try {
+            PostEntity createdPost = postService.createPost(requestPost);
+            return responseBuilder.buildSuccessResponse(createdPost);
+        } catch (EntityNotFoundException e) {
+            return responseBuilder.buildErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return responseBuilder.buildErrorResponse("An unexpected error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
