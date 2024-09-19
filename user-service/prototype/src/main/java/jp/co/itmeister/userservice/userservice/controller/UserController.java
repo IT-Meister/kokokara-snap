401b1c0f326a4a4dcd69dd4381aa5eb2dfb4904d
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-20 01:09:16 +0900

    [add] update user func in controller.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/UserController.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/controller/UserController.java
@@ -5,11 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import jp.co.itmeister.userservice.userservice.dto.UserResponseDto;
+import jp.co.itmeister.userservice.userservice.dto.UserUpdateRequestDto;
 import jp.co.itmeister.userservice.userservice.entity.UserEntity;
 import jp.co.itmeister.userservice.userservice.responseBuilder.ResponseBuilder;
 import jp.co.itmeister.userservice.userservice.service.UserService;
 
 import java.util.Optional;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -24,7 +29,7 @@ public class UserController {
     }
 
     @GetMapping("/{user_id}")
-    public ResponseEntity<?> showUser(@PathVariable("user_id") Long id) {
+    public ResponseEntity<Map<String , Object>> showUser(@PathVariable("user_id") Long id) {
         Optional<UserEntity> userOptional = userService.showUser(id);
 
         if(userOptional.isPresent()) {
@@ -34,5 +39,17 @@ public class UserController {
         }
     }
 
+    @PutMapping("/{user_id}")
+    public ResponseEntity<Map<String , Object>> updateUser (@PathVariable("user_id") Long id , @Valid @RequestBody UserUpdateRequestDto editUser) {
+        try {
+            UserResponseDto editedUser = userService.updateUser(id, editUser);
+            return responseBuilder.buildSuccessResponse(editedUser);
+        } catch (EntityNotFoundException e) {
+            return responseBuilder.buildErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e) {
+            return responseBuilder.buildErrorResponse("User udpate falied.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
