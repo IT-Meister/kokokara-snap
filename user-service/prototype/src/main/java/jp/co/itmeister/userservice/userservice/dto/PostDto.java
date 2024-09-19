02c5a78da2ae70643c7b053b734a869f3ad88695
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-19 23:05:30 +0900

    [add] setter in post dto.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/dto/PostDto.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/dto/PostDto.java
@@ -1,8 +1,14 @@
 package jp.co.itmeister.userservice.userservice.dto;
 
 import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.math.BigDecimal;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostDto {
     private Integer userId;
     private String url;
@@ -17,6 +23,8 @@ public class PostDto {
     private ZonedDateTime snapTime;
     private Integer angle;
     private String iso;
+
+    @JsonProperty("f_value")
     private BigDecimal fValue;
     private String shutterSpeed;
 
@@ -48,60 +56,120 @@ public class PostDto {
         return userId;
     }
 
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
     public String getUrl() {
         return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public Short getPrefecture() {
         return prefecture;
+    }
+
+    public void setPrefecture(Short prefecture) {
+        this.prefecture = prefecture;
     }
 
     public String getCityName() {
         return cityName;
     }
 
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getBrand() {
         return brand;
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public String getCameraName() {
         return cameraName;
+    }
+
+    public void setCameraName(String cameraName) {
+        this.cameraName = cameraName;
     }
 
     public BigDecimal getLatitude() {
         return latitude;
     }
 
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
     public BigDecimal getLongitude() {
         return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
     }
 
     public ZonedDateTime getSnapTime() {
         return snapTime;
     }
 
+    public void setSnapTime(ZonedDateTime snapTime) {
+        this.snapTime = snapTime;
+    }
+
     public Integer getAngle() {
         return angle;
+    }
+
+    public void setAngle(Integer angle) {
+        this.angle = angle;
     }
 
     public String getIso() {
         return iso;
     }
 
+    public void setIso (String iso) {
+        this.iso = iso;
+    }
+
     public BigDecimal getFValue() {
         return fValue;
     }
 
+    public void setFValue(BigDecimal fValue) {
+        this.fValue = fValue;
+    }
+
     public String getShutterSpeed() {
         return shutterSpeed;
+    }
+
+    public void setShutterSpeed(String shutterSpeed) {
+        this.shutterSpeed = shutterSpeed;
     }
 
 }
