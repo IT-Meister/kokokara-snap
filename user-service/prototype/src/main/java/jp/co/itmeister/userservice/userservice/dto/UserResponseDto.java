7d9a092bc82d9b85ec2c63065af6db82f6718247
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-20 01:10:18 +0900

    [fix] convert to snake case.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/dto/UserResponseDto.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/dto/UserResponseDto.java
@@ -2,13 +2,17 @@ package jp.co.itmeister.userservice.userservice.dto;
 
 import jp.co.itmeister.userservice.userservice.entity.UserEntity;
 
-public class UserResponseDto {
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)public class UserResponseDto {
     
     private Long id;
     private String userName;
     private String displayName;
     private String email;
     private Short prefecture;
+    private String iconUrl;
 
     public UserResponseDto (UserEntity user) {
         this.id = user.getId();
@@ -16,6 +20,7 @@ public class UserResponseDto {
         this.displayName = user.getDisplayName();
         this.email = user.getEmail();
         this.prefecture = user.getPrefecture();
+        this.iconUrl = user.getIconUrl();
     }
 
         // Getters
@@ -24,4 +29,5 @@ public class UserResponseDto {
     public String getDisplayName() { return displayName; }
     public String getEmail() { return email; }
     public Short getPrefecture() { return prefecture; }
+    public String getIconUrl() {return iconUrl;}
 }
