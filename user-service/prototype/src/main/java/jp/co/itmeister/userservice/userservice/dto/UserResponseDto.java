1b312e1943a491b8e13b1cda16b6128555d3f3e9
kijima-riku <riku.bulls.0116@icloud.com>
2024-10-20 05:58:56 +0900

    [add] upload and fullPath method.

--- user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/dto/UserResponseDto.java
+++ user-service/prototype/src/main/java/jp/co/itmeister/userservice/userservice/dto/UserResponseDto.java
@@ -1,11 +1,11 @@
 package jp.co.itmeister.userservice.userservice.dto;
 
-import jp.co.itmeister.userservice.userservice.entity.UserEntity;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)public class UserResponseDto {
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserResponseDto {
     
     private Long id;
     private String userName;
@@ -14,14 +14,7 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
     private Short prefecture;
     private String iconUrl;
 
-    public UserResponseDto (UserEntity user) {
-        this.id = user.getId();
-        this.userName = user.getUserName();
-        this.displayName = user.getDisplayName();
-        this.email = user.getEmail();
-        this.prefecture = user.getPrefecture();
-        this.iconUrl = user.getIconUrl();
-    }
+    public UserResponseDto(){};
 
         // Getters
     public Long getId() { return id; }
@@ -30,4 +23,11 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
     public String getEmail() { return email; }
     public Short getPrefecture() { return prefecture; }
     public String getIconUrl() {return iconUrl;}
+
+    public void setId(Long id) { this.id = id; }
+    public void setUserName(String userName) { this.userName = userName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPrefecture(Short prefecture) { this.prefecture = prefecture; }
+    public void setIconUrl(String iconUrl) { this.iconUrl = iconUrl; }
 }
